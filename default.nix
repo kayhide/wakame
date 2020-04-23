a034304e6430c72424c362993430ada432f9865c
Hideaki Kawai <kayhide@gmail.com>
2020-04-23 13:27:44 +0900

    Pin haskell.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 let
-  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {};
+  rev = "0ef4b08b96cc6d57943a9fdc9b4499183b299f11"; # 2020-04-23 01:17:07
+  haskellNix = import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/${rev}.tar.gz") {};
   nixpkgsSrc = haskellNix.sources.nixpkgs-1909;
   nixpkgsArgs = haskellNix.nixpkgsArgs;
 
