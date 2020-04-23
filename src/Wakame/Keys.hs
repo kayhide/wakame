d5b5272925e2da8bb7b0409a139d4fd8c65d8d88
Hideaki Kawai <kayhide@gmail.com>
2020-04-23 16:03:58 +0900

    Update keys to return IsString instances

--- src/Wakame/Keys.hs
+++ src/Wakame/Keys.hs
@@ -4,6 +4,7 @@ module Wakame.Keys where
 import Prelude
 
 import Data.Proxy
+import Data.String (IsString (..))
 import GHC.Generics
 import GHC.TypeLits
 import Wakame.Rec (Keyed (..))
@@ -13,27 +14,32 @@ import Wakame.Rec (Keyed (..))
 -- >>> import Wakame.Examples
 
 
--- |
+-- | Function to retrieve key values
 -- >>> keys pt
 -- ["x","y"]
-keys :: (Generic a, Keys' (Rep a)) => a -> [String]
+--
+-- Return value can be anything which has @IsString@ instance.
+-- >>> import Data.Functor.Identity (Identity)
+-- >>> keys pt :: [Identity String]
+-- [Identity "x",Identity "y"]
+keys :: (IsString s, Generic a, Keys' (Rep a) s) => a -> [s]
 keys = keys' . from
 
 
-class Keys' f where
-  keys' :: f a -> [String]
+class IsString s => Keys' f s where
+  keys' :: f a -> [s]
 
-instance Keys' a => Keys' (D1 f a) where
+instance Keys' a s => Keys' (D1 f a) s where
   keys' (M1 x) = keys' x
 
-instance Keys' a => Keys' (C1 f a) where
+instance Keys' a s => Keys' (C1 f a) s where
   keys' (M1 x) = keys' x
 
-instance (Keys' a, Keys' b) => Keys' (a :*: b) where
+instance (Keys' a s, Keys' b s) => Keys' (a :*: b) s where
   keys' (x :*: y) = keys' x <> keys' y
 
-instance KnownSymbol key => Keys' (S1 ('MetaSel ('Just key) su ss ds) a) where
-  keys' _ = [symbolVal (Proxy @key)]
+instance (KnownSymbol key, IsString s) => Keys' (S1 ('MetaSel ('Just key) su ss ds) a) s where
+  keys' _ = [fromString $ symbolVal (Proxy @key)]
 
-instance KnownSymbol key => Keys' (S1 ('MetaSel 'Nothing su ss ds) (Rec0 (Keyed key a))) where
-  keys' _ = [symbolVal (Proxy @key)]
+instance (KnownSymbol key, IsString s) => Keys' (S1 ('MetaSel 'Nothing su ss ds) (Rec0 (Keyed key a))) s where
+  keys' _ = [fromString $ symbolVal (Proxy @key)]
