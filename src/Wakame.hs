fd8a1024acd67b1513b93150eaa847db52a4c988
Hideaki Kawai <kayhide@gmail.com>
2020-04-23 13:46:52 +0900

    Move Row to IsRec

--- src/Wakame.hs
+++ src/Wakame.hs
@@ -2,8 +2,8 @@ module Wakame
   ( module X
   ) where
 
+import Wakame.IsRec as X
 import Wakame.Keys as X
 import Wakame.Nub as X
 import Wakame.Rec as X
-import Wakame.Row as X
 import Wakame.Union as X
