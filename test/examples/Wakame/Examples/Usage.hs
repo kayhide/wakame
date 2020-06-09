327e430be384e6dc44a9e0a0dc60cddb416e13e6
Hideaki Kawai <kayhide@gmail.com>
2020-06-09 09:56:42 +0900

    Add contstraint to Union typeclass

--- test/examples/Wakame/Examples/Usage.hs
+++ test/examples/Wakame/Examples/Usage.hs
@@ -1,14 +1,12 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 module Wakame.Examples.Usage where
 
 import Prelude
 
-import Data.Kind
-import Data.Proxy
 import Data.Text (Text)
-import Data.Time (UTCTime)
+import Data.Time (UTCTime, getCurrentTime)
 import GHC.Generics
-import GHC.TypeLits
 import Wakame
 
 
@@ -50,3 +48,47 @@ touchUser time user = fromRow $ nub $ union (toRow $ UpdatedAt time) (toRow user
 updateAndTouchUser :: UpdatingUser -> UTCTime -> User -> User
 updateAndTouchUser updating time user =
   fromRow $ nub $ union (toRow updating) $ union (toRow $ UpdatedAt time) (toRow user)
+
+
+
+newtype ID a = ID Int
+  deriving (Eq, Show, Generic)
+
+data ModelBase a =
+  ModelBase
+  { id         :: ID a
+  , created_at :: !UTCTime
+  , updated_at :: !UTCTime
+  }
+  deriving (Eq, Show, Generic)
+
+
+create ::
+  forall a b attrs.
+  ( IsRow a
+  , IsRow b
+  , Lacks "id" (Of a)
+  , Union (Of a) (Of (ModelBase b)) attrs
+  , Nub attrs (Of b)
+  ) => a -> IO b
+create x = do
+  now <- getCurrentTime
+  let y =
+        fromRow $ nub
+        $ union (toRow x)
+        $ toRow $ ModelBase (ID @b 0) now now
+  pure y
+
+
+update ::
+  ( IsRow a
+  , Union (Of UpdatedAt) (Of a) attrs
+  , Nub attrs (Of a)
+  ) => a -> IO a
+update x = do
+  now <- getCurrentTime
+  let y =
+        fromRow $ nub
+        $ union (toRow $ UpdatedAt now)
+        $ toRow x
+  pure y
